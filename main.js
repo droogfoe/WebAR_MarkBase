53d7d9642bf87dc801804249999f93f6273a226d
Anders_sen <anders8729@gmail.com>
2021-03-09 19:46:52 +0900

    Add java scrip array for testing

---
+++ main.js
@@ -0,0 +1,28 @@
+// import { datas } from "./modelsList.js";
+
+// (function()
+// {
+//     "use strict"
+
+//     $(document).ready(init);
+
+//     function init()
+//     {
+//         var config = 
+//         {
+//             scalefig: 0,
+//             isVolume: true.value,
+//             currentOnject: {},
+//             currentId: 0,
+//             prev: 0
+//         };
+//         function AddObject(datas, toRemove, toRemoveId)
+//         {
+//             let currentData = datas[config.currentId];
+//             if(document.querySelector("#obj_" + currentData.id))
+//             {
+//                 document.querySelector
+//             }
+//         }
+//     }
+// })();
---
+++ modelsList.js
@@ -0,0 +1,17 @@
+// export const datas
+// [
+//     {
+//         id: 1,
+//         title: "Nobuna",
+//         description: "Nobuna is a charactor from 'Monogatari'.",
+//         scale: "2 2 2",
+//         isObj: true
+//     },
+//     {
+//         id: 2,
+//         title: "Knee",
+//         description: "Knee is a charactor from 'Genshin Impact'.",
+//         scale: "2 2 2",
+//         isObj: true
+//     }
+// ];
